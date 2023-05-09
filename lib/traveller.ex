6ecc189c873c1323e45970855772a387de355589
Hugo <hpjm37@gmail.com>
2023-05-09 19:04:44 +0100

    Add basic cursor-based stream and some tests

--- lib/traveller.ex
+++ lib/traveller.ex
@@ -1,18 +1,57 @@
 defmodule Traveller do
-  @moduledoc """
-  Documentation for `Traveller`.
-  """
+  import Ecto.Query
 
-  @doc """
-  Hello world.
+  def run(opts) do
+    schema = Keyword.fetch!(opts, :schema)
+    cursor = Keyword.get(opts, :cursor, :id)
+    repo = Keyword.fetch!(opts, :repo)
+    chunk_size = Keyword.get(opts, :chunk_size, 100)
 
-  ## Examples
+    start_after =
+      Keyword.get_lazy(opts, :start_after, fn ->
+        # Make this smarter eventually
+        0
+      end)
 
-      iex> Traveller.hello()
-      :world
+    next_cursor =
+      Keyword.get(opts, :next_cursor, fn results ->
+        results |> List.last() |> Map.get(cursor)
+      end)
 
-  """
-  def hello do
-    :world
+    Stream.unfold(
+      %{
+        chunk_size: chunk_size,
+        cursor: cursor,
+        next_cursor: next_cursor,
+        repo: repo,
+        schema: schema,
+        start_after: start_after
+      },
+      &iterate/1
+    )
+  end
+
+  defp iterate(
+         params = %{
+           chunk_size: chunk_size,
+           cursor: cursor,
+           next_cursor: next_cursor,
+           repo: repo,
+           schema: schema,
+           start_after: start_after
+         }
+       ) do
+    schema
+    |> where([s], field(s, ^cursor) > ^start_after)
+    |> order_by(asc: ^cursor)
+    |> limit(^chunk_size)
+    |> repo.all()
+    |> case do
+      [] ->
+        nil
+
+      results ->
+        {results, %{params | start_after: next_cursor.(results)}}
+    end
   end
 end
--- test/traveller_test.exs
+++ test/traveller_test.exs
@@ -1,8 +1,65 @@
 defmodule TravellerTest do
-  use ExUnit.Case
-  doctest Traveller
+  use Traveller.RepoCase
+  alias Traveller.Person
+  alias Traveller.TestRepo
 
-  test "greets the world" do
-    assert Traveller.hello() == :world
+  describe "cursor mode" do
+    setup do
+      dumbledore =
+        TestRepo.insert!(%Person{first_name: "Albus", last_name: "Dumbledore", age: 1001})
+
+      snape = TestRepo.insert!(%Person{first_name: "Severus", last_name: "Snape", age: 42})
+
+      indiana_jones =
+        TestRepo.insert!(%Person{first_name: "Indiana", last_name: "Jones", age: 59})
+
+      batman = TestRepo.insert!(%Person{first_name: "Bruce", last_name: "Wayne", age: 37})
+
+      %{
+        dumbledore: dumbledore,
+        snape: snape,
+        indiana_jones: indiana_jones,
+        batman: batman,
+        all: [dumbledore, snape, indiana_jones, batman]
+      }
+    end
+
+    test "default cursor is id", %{all: all} do
+      stream = Traveller.run(repo: TestRepo, schema: Person)
+      assert Enum.take(stream, 1) == [all]
+    end
+
+    test "chunk size is configurable", %{
+      dumbledore: dumbledore,
+      snape: snape,
+      indiana_jones: indiana_jones,
+      batman: batman
+    } do
+      stream = Traveller.run(repo: TestRepo, schema: Person, chunk_size: 1)
+      assert Enum.take(stream, 1) == [[dumbledore]]
+      assert Enum.take(stream, 2) == [[dumbledore], [snape]]
+      assert Enum.take(stream, 3) == [[dumbledore], [snape], [indiana_jones]]
+      assert Enum.take(stream, 4) == [[dumbledore], [snape], [indiana_jones], [batman]]
+    end
+
+    test "start_after is configurable", %{
+      indiana_jones: indiana_jones,
+      batman: batman
+    } do
+      stream = Traveller.run(repo: TestRepo, schema: Person, start_after: indiana_jones.id)
+      assert Enum.take(stream, 1) == [[batman]]
+    end
+
+    test "cursor field is configurable", %{
+      indiana_jones: indiana_jones,
+      dumbledore: dumbledore,
+      snape: snape,
+      batman: batman
+    } do
+      stream =
+        Traveller.run(repo: TestRepo, schema: Person, cursor: :first_name, start_after: "A")
+
+      assert Enum.take(stream, 1) == [[dumbledore, batman, indiana_jones, snape]]
+    end
   end
 end
