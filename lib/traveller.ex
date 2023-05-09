a0a6d01a0ef99891363391f6e3151acdc3375635
Hugo <hpjm37@gmail.com>
2023-05-09 19:20:59 +0100

    Add offset mode

--- lib/traveller.ex
+++ lib/traveller.ex
@@ -3,32 +3,40 @@ defmodule Traveller do
 
   def run(opts) do
     schema = Keyword.fetch!(opts, :schema)
-    cursor = Keyword.get(opts, :cursor, :id)
     repo = Keyword.fetch!(opts, :repo)
     chunk_size = Keyword.get(opts, :chunk_size, 100)
+    mode = Keyword.get(opts, :mode, :cursor)
 
-    start_after =
-      Keyword.get_lazy(opts, :start_after, fn ->
-        # Make this smarter eventually
-        0
-      end)
+    base_opts = %{schema: schema, repo: repo, chunk_size: chunk_size, mode: mode}
 
-    next_cursor =
-      Keyword.get(opts, :next_cursor, fn results ->
-        results |> List.last() |> Map.get(cursor)
-      end)
+    opts =
+      if mode == :offset do
+        Map.merge(base_opts, %{
+          offset: Keyword.get(opts, :initial_offset, 0),
+          sort_key: Keyword.get(opts, :sort_key, :id)
+        })
+      else
+        cursor = Keyword.get(opts, :cursor, :id)
 
-    Stream.unfold(
-      %{
-        chunk_size: chunk_size,
-        cursor: cursor,
-        next_cursor: next_cursor,
-        repo: repo,
-        schema: schema,
-        start_after: start_after
-      },
-      &iterate/1
-    )
+        start_after =
+          Keyword.get_lazy(opts, :start_after, fn ->
+            # Make this smarter eventually
+            0
+          end)
+
+        next_cursor =
+          Keyword.get(opts, :next_cursor, fn results ->
+            results |> List.last() |> Map.get(cursor)
+          end)
+
+        Map.merge(base_opts, %{
+          cursor: cursor,
+          start_after: start_after,
+          next_cursor: next_cursor
+        })
+      end
+
+    Stream.unfold(opts, &iterate/1)
   end
 
   defp iterate(
@@ -38,7 +46,8 @@ defmodule Traveller do
            next_cursor: next_cursor,
            repo: repo,
            schema: schema,
-           start_after: start_after
+           start_after: start_after,
+           mode: :cursor
          }
        ) do
     schema
@@ -52,6 +61,30 @@ defmodule Traveller do
 
       results ->
         {results, %{params | start_after: next_cursor.(results)}}
+    end
+  end
+
+  defp iterate(
+         params = %{
+           chunk_size: chunk_size,
+           repo: repo,
+           schema: schema,
+           offset: offset,
+           mode: :offset,
+           sort_key: sort_key
+         }
+       ) do
+    schema
+    |> order_by(asc: ^sort_key)
+    |> limit(^chunk_size)
+    |> offset(^offset)
+    |> repo.all()
+    |> case do
+      [] ->
+        nil
+
+      results ->
+        {results, %{params | offset: offset + chunk_size}}
     end
   end
 end
--- test/traveller_test.exs
+++ test/traveller_test.exs
@@ -3,27 +3,26 @@ defmodule TravellerTest do
   alias Traveller.Person
   alias Traveller.TestRepo
 
+  setup do
+    dumbledore =
+      TestRepo.insert!(%Person{first_name: "Albus", last_name: "Dumbledore", age: 1001})
+
+    snape = TestRepo.insert!(%Person{first_name: "Severus", last_name: "Snape", age: 42})
+
+    indiana_jones = TestRepo.insert!(%Person{first_name: "Indiana", last_name: "Jones", age: 59})
+
+    batman = TestRepo.insert!(%Person{first_name: "Bruce", last_name: "Wayne", age: 37})
+
+    %{
+      dumbledore: dumbledore,
+      snape: snape,
+      indiana_jones: indiana_jones,
+      batman: batman,
+      all: [dumbledore, snape, indiana_jones, batman]
+    }
+  end
+
   describe "cursor mode" do
-    setup do
-      dumbledore =
-        TestRepo.insert!(%Person{first_name: "Albus", last_name: "Dumbledore", age: 1001})
-
-      snape = TestRepo.insert!(%Person{first_name: "Severus", last_name: "Snape", age: 42})
-
-      indiana_jones =
-        TestRepo.insert!(%Person{first_name: "Indiana", last_name: "Jones", age: 59})
-
-      batman = TestRepo.insert!(%Person{first_name: "Bruce", last_name: "Wayne", age: 37})
-
-      %{
-        dumbledore: dumbledore,
-        snape: snape,
-        indiana_jones: indiana_jones,
-        batman: batman,
-        all: [dumbledore, snape, indiana_jones, batman]
-      }
-    end
-
     test "default cursor is id", %{all: all} do
       stream = Traveller.run(repo: TestRepo, schema: Person)
       assert Enum.take(stream, 1) == [all]
@@ -60,6 +59,43 @@ defmodule TravellerTest do
         Traveller.run(repo: TestRepo, schema: Person, cursor: :first_name, start_after: "A")
 
       assert Enum.take(stream, 1) == [[dumbledore, batman, indiana_jones, snape]]
+    end
+  end
+
+  describe "offset mode" do
+    test "default fetches 100", %{all: all} do
+      stream = Traveller.run(repo: TestRepo, schema: Person, mode: :offset)
+      assert Enum.take(stream, 1) == [all]
+    end
+
+    test "chunk size is configurable", %{
+      snape: snape,
+      dumbledore: dumbledore,
+      batman: batman,
+      indiana_jones: indiana_jones
+    } do
+      stream = Traveller.run(repo: TestRepo, schema: Person, mode: :offset, chunk_size: 2)
+      assert Enum.take(stream, 2) == [[dumbledore, snape], [indiana_jones, batman]]
+    end
+
+    test "initial_offset is configurable", %{
+      snape: snape,
+      dumbledore: dumbledore,
+      batman: batman,
+      indiana_jones: indiana_jones
+    } do
+      stream = Traveller.run(repo: TestRepo, schema: Person, mode: :offset, initial_offset: 2)
+      assert Enum.take(stream, 1) == [[indiana_jones, batman]]
+    end
+
+    test "sort_key is configurable", %{
+      snape: snape,
+      dumbledore: dumbledore,
+      batman: batman,
+      indiana_jones: indiana_jones
+    } do
+      stream = Traveller.run(repo: TestRepo, schema: Person, mode: :offset, sort_key: :last_name)
+      assert Enum.take(stream, 1) == [[dumbledore, indiana_jones, snape, batman]]
     end
   end
 end
