3ac8755914ff79eec88c4009122c5576658412be
Hugo <hpjm37@gmail.com>
2023-05-15 08:51:29 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Traveller.MixProject do
   defp deps do
     [
       {:ecto_sql, "~> 3.10"},
-      {:postgrex, "~> 0.17", only: [:test]}
+      {:postgrex, "~> 0.17", only: [:test]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
