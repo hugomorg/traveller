3d9d8894b9cf0fdaf7fdfdd71365f802d001db1a
Hugo <hpjm37@gmail.com>
2023-05-16 19:43:58 +0100

    Increment version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Traveller.MixProject do
   def project do
     [
       app: :traveller,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
